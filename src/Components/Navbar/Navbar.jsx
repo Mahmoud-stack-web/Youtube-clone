35289cfe8d02484ba623f8e088c52acc4d604acf
Mahmoud <horsewhite446@gmail.com>
2025-07-19 14:49:28 +0300

    Added react icons and finalized navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,32 @@
 import "./Navbar.css";
-import menu_icon from "../../assets/menu.png";
-import logo from "../../assets/logo.png";
-import search_icon from "../../assets/search.png";
-import upload_icon from "../../assets/upload.png";
-import more_icon from "../../assets/more.png";
-import notification_icon from "../../assets/notification.png";
-import profile_icon from "../../assets/jack.png";
+import { VscMenu } from "react-icons/vsc";
+import { IoLogoYoutube } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
+import { RiVideoUploadLine } from "react-icons/ri";
+import { IoIosMore, IoIosMic } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+
 function Navbar() {
   return (
-    <nav className="flex-div">
+    <nav className={`flex-div `}>
       <div className="nav-left flex-div">
-        <img className="menu-icon" src={menu_icon} />
-        <img className="logo" src={logo} />
+        <VscMenu className="menu-icon" />
+        <IoLogoYoutube className="logo" />
+        <span className="logo-text">YouTube</span>
       </div>
+
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">
           <input type="text" placeholder="Search" />
-          <img src={search_icon} />
+          <CiSearch className="search-icon" />
         </div>
+        <IoIosMic className="mic-icon" />
       </div>
+
       <div className="nav-right flex-div">
-        <img src={upload_icon} />
-        <img src={more_icon} />
-        <img src={notification_icon} />
-        <img src={profile_icon} className="user-icon" />
+        <RiVideoUploadLine className="nav-icon" />
+        <IoIosMore className="nav-icon" />
+        <CgProfile className="user-icon" />
       </div>
     </nav>
   );
