a1006dce10132fcfed4b0b59fa196c2c62b421a4
Mahmoud <horsewhite446@gmail.com>
2025-07-28 15:55:46 +0300

    did a lot of changes to the sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,59 +1,82 @@
 import "./Sidebar.css";
 import { IoMdHome } from "react-icons/io";
 import { IoGameController } from "react-icons/io5";
-import { FaCarSide } from "react-icons/fa";
+import { FaBlog, FaCarSide } from "react-icons/fa";
 import { FaBasketballBall } from "react-icons/fa";
 import { FiTv } from "react-icons/fi";
 import { GrTechnology } from "react-icons/gr";
 import { MdLibraryMusic } from "react-icons/md";
-import blogs from "../../assets/blogging.png";
 import { IoNewspaperOutline } from "react-icons/io5";
 import jack from "../../assets/jack.png";
 import simon from "../../assets/simon.png";
 import tom from "../../assets/tom.png";
 import megan from "../../assets/megan.png";
 import cameron from "../../assets/cameron.png";
-const Sidebar = ({ sidebar }) => {
+const Sidebar = ({ sidebar, category, setCategory }) => {
   return (
     <div className={`sidebar ${sidebar ? "" : "small-sidebar"}`}>
       <div className="shortcut-links">
-        <div className="side-link">
+        <div
+          data-tooltip="Home"
+          onClick={() => setCategory(0)}
+          className={`side-link ${category === 0 ? "active" : ""}`}
+        >
           <IoMdHome size={22} />
           <p>Home</p>
         </div>
-        <div className="side-link">
+        <div
+          onClick={() => setCategory(20)}
+          className={`side-link ${category === 20 ? "active" : ""}`}
+        >
           <IoGameController size={22} />
           <p>Gaming</p>
         </div>
-        <div className="side-link">
+        <div
+          onClick={() => setCategory(2)}
+          className={`side-link ${category === 2 ? "active" : ""}`}
+        >
           <FaCarSide size={22} />
           <p>Automobiles</p>
         </div>
-        <div className="side-link">
+        <div
+          onClick={() => setCategory(17)}
+          className={`side-link ${category === 17 ? "active" : ""}`}
+        >
           <FaBasketballBall size={22} />
           <p>Sports</p>
         </div>
-        <div className="side-link">
+        <div
+          onClick={() => setCategory(24)}
+          className={`side-link ${category === 24 ? "active" : ""}`}
+        >
           <FiTv size={22} />
           <p>Entertainment</p>
         </div>
-        <div className="side-link">
+        <div
+          onClick={() => setCategory(28)}
+          className={`side-link ${category === 28 ? "active" : ""}`}
+        >
           <GrTechnology size={22} />
           <p>Technology</p>
         </div>
-        <div className="side-link">
+        <div
+          onClick={() => setCategory(10)}
+          className={`side-link ${category === 10 ? "active" : ""}`}
+        >
           <MdLibraryMusic size={22} />
           <p>Music</p>
         </div>
-        <div className="side-link">
-          <img
-            style={{ width: "22px", height: "22px" }}
-            src={blogs}
-            alt="blogs"
-          />
+        <div
+          onClick={() => setCategory(22)}
+          className={`side-link ${category === 22 ? "active" : ""}`}
+        >
+          <FaBlog size={22} />
           <p>Blogs</p>
         </div>
-        <div className="side-link">
+        <div
+          onClick={() => setCategory(25)}
+          className={`side-link ${category === 25 ? "active" : ""}`}
+        >
           <IoNewspaperOutline size={22} />
           <p>News</p>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,18 @@
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import "./Home.css";
 import Feed from "../../Components/Feed/Feed";
+import { useState } from "react";
 function Home({ sidebar }) {
+  const [category, setCategory] = useState(0);
   return (
     <div className="home-layout">
-      <Sidebar sidebar={sidebar}></Sidebar>
+      <Sidebar
+        sidebar={sidebar}
+        category={category}
+        setCategory={setCategory}
+      ></Sidebar>
       <div className={`container ${sidebar ? "" : "large-container"}`}>
-        <Feed />
+        <Feed category={category} />
       </div>
     </div>
   );
