d4670e2dd4cd33bd2dff100543d08214e1df1cb2
Mahmoud <horsewhite446@gmail.com>
2025-07-29 05:34:17 +0300

    fixed tiny mistake

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,12 +1,4 @@
 import "./Feed.css";
-import thumbnail1 from "../../assets/thumbnail1.png";
-import thumbnail2 from "../../assets/thumbnail2.png";
-import thumbnail3 from "../../assets/thumbnail3.png";
-import thumbnail4 from "../../assets/thumbnail4.png";
-import thumbnail5 from "../../assets/thumbnail5.png";
-import thumbnail6 from "../../assets/thumbnail6.png";
-import thumbnail7 from "../../assets/thumbnail7.png";
-import thumbnail8 from "../../assets/thumbnail8.png";
 import { Link } from "react-router-dom";
 import { API_KEY, value_converter } from "../../data";
 import { useEffect, useState } from "react";
@@ -24,7 +16,7 @@ const Feed = ({ category }) => {
   }, [category]);
   return (
     <div className="feed">
-      {data.map((item, index) => {
+      {data.map((item) => {
         return (
           <Link
             to={`video/${item.snippet.categoryId}/${item.id}`}
